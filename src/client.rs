fd91f2887092ec72e2843e948bcacaea88abde9d
Friedrich482 <friedricht109@gmail.com>
2026-06-15 14:01:49 +0200

    feat: client
    - added the `client` struct and utility functions

--- src/client.rs
+++ src/client.rs
@@ -30,45 +30,66 @@ pub struct Client {
 
 impl Client {
     /// Cree un nouveau client avec des secrets x, c frais.
-    ///
-    /// TODO (etape 1 de Fig. 3) :
-    ///   - tirer x et c aleatoirement avec crypto::random_scalar(rng)
-    ///   - retourner un Client avec ces secrets, kcs = None, kcfs = None
     pub fn new(pk_fw: RistrettoPoint, pk_server: VerifyingKey, rng: &mut impl RngCore) -> Self {
-        todo!("Generer x et c aleatoirement (crypto::random_scalar), construire le Client")
+        let x = crypto::random_scalar(rng);
+        let c = crypto::random_scalar(rng);
+
+        Client {
+            x,
+            c,
+            pk_fw,
+            pk_server,
+            kcs: None,
+            kcfs: None,
+        }
     }
 
     /// Construit le premier message envoye au Firewall : (X, C, e).
-    ///
-    /// Reference : Fig. 3, premiere fleche "Client -> Firewall".
-    ///
-    /// TODO :
-    ///   1. Calculer X = g^x  -> crypto::base_point(&self.x)
-    ///   2. Calculer C = g^c  -> crypto::base_point(&self.c)
-    ///   3. Chiffrer c pour le firewall :
-    ///        e = crypto::elgamal_encrypt(&self.pk_fw, &self.c.to_bytes(), rng)
-    ///   4. Retourner ClientInit { big_x: X, big_c: C, enc_c: e }
     pub fn init_message(&self, rng: &mut impl RngCore) -> ClientInit {
-        todo!("Construire (X, C, e)")
+        // Etape 1 : X = g^x
+        let big_x = crypto::base_point(&self.x);
+
+        // Etape 2 : C = g^c
+        let big_c = crypto::base_point(&self.c);
+
+        // Etape 3 : e = ElGamal(pkFW, c·G)
+        // On chiffre le point C lui-meme (= c·G), le firewall pourra
+        // verifier apres dechiffrement que le resultat est bien egal a C.
+        let enc_c = crypto::elgamal_encrypt(&self.pk_fw, &self.c.to_bytes(), rng);
+
+        ClientInit {
+            big_x,
+            big_c,
+            enc_c,
+        }
     }
 
     /// Traite la reponse finale du Firewall : (sigma, Y, D, gamma1, gamma2).
-    ///
-    /// Reference : Fig. 3, derniere fleche "Firewall -> Client".
-    ///
-    /// TODO, doit :
-    ///   1. Reconstruire le transcript signe : (Y, D, X^gamma1, C^gamma2)
-    ///      Note : X^gamma1 = crypto::base_point(&(self.x * msg.gamma1))
-    ///             C^gamma2 = crypto::base_point(&(self.c * msg.gamma2))
-    ///   2. Verifier la signature avec self.pk_server.verify(&transcript_bytes, &msg.signature)
-    ///      -> crypto::concat_points(&[&msg.big_y, &msg.big_d, &x_gamma1, &c_gamma2])
-    ///   3. Si la signature est invalide : retourner Err(...) (abort, cf Fig. 3 "Else: abort")
-    ///   4. Si valide :
-    ///        - kcs_point  = Y^(x*gamma1)  = (self.x * msg.gamma1) * msg.big_y
-    ///        - kcfs_point = D^(c*gamma2)  = (self.c * msg.gamma2) * msg.big_d
-    ///        - self.kcs  = Some(crypto::kdf(&kcs_point))
-    ///        - self.kcfs = Some(crypto::kdf(&kcfs_point))
     pub fn finalize(&mut self, msg: FirewallToClient) -> Result<(), String> {
-        todo!("Verifier sigma puis calculer kcs et kcfs")
+        // Etape 1 : reconstruire le transcript signe (Y, D, X^gamma1, C^gamma2)
+        // X^gamma1 = g^(x*gamma1) car X = g^x
+        let x_gamma1 = crypto::base_point(&(self.x * msg.gamma1));
+        // C^gamma2 = g^(c*gamma2) car C = g^c
+        let c_gamma2 = crypto::base_point(&(self.c * msg.gamma2));
+
+        let transcript_bytes =
+            crypto::concat_points(&[&msg.big_y, &msg.big_d, &x_gamma1, &c_gamma2]);
+
+        // Etape 2 : verifier la signature
+        // Si invalide -> abort (cf Fig. 3 "Else: abort")
+        self.pk_server
+            .verify(&transcript_bytes, &msg.signature)
+            .map_err(|e| format!("Signature invalide : {}", e))?;
+
+        // Etape 3 : calculer kcs et kcfs
+        // kcs  = Y^(x*gamma1)
+        let kcs_point = (self.x * msg.gamma1) * msg.big_y;
+        // kcfs = D^(c*gamma2)
+        let kcfs_point = (self.c * msg.gamma2) * msg.big_d;
+
+        self.kcs = Some(crypto::kdf(&kcs_point));
+        self.kcfs = Some(crypto::kdf(&kcfs_point));
+
+        Ok(())
     }
 }
